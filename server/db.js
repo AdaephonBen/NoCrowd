e999f1cce69277b5324d69008c92236fb8c14a14
Vishnu VS <ma18btech11011@iith.ac.in>
2022-02-27 23:05:44 +0530

    routes done

--- server/db.js
+++ server/db.js
@@ -55,7 +55,7 @@ const getStatus = async (email, orderid) => {
 
 const getOrders = async () => {
   const res1 = await pool.query(
-    'SELECT b.id AS id, a.name AS name, b.quantity AS quantity, b.status AS status FROM public."Item" AS a, public."Order" AS b WHERE a.id=b.item_id and caterer_id=1'
+    'SELECT b.id AS id, a.name AS name, b.quantity AS quantity, b.status AS status FROM public."Item" AS a, public."Order" AS b WHERE a.id=b.item_id and b.caterer_id=1'
   );
   return res1.rows;
 };
